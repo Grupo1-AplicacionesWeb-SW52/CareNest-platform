8511ae05644ce29a908c0d2c79bb254ce711fa72
RenzoA <u202113612@upc.edu.pe>
2024-11-21 23:00:42 -0500

    feat(paymentMethod): CreateUserPaymentCommand.cs is created.

--- WebApplication3/user-payment-methods/Domain/Model/Commands/CreateUserPaymentCommand.cs
+++ WebApplication3/user-payment-methods/Domain/Model/Commands/CreateUserPaymentCommand.cs
@@ -1,9 +1,10 @@
 namespace WebApplication3.user_payment_methods.Domain.Model.Commands;
 
 public record CreateUserPaymentCommand(
-    int UserId,
     string CardNumber,
     DateTime ExpirationDate,
     string Cvv,
-    string CardHolder
+    string CardHolder,
+    int? CaregiverId = null,
+    int? TutorId = null
     );
