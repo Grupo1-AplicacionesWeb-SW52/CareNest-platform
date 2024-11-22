e4758476986ebd1db51d2c417c0d977f3c01f750
RenzoA <u202113612@upc.edu.pe>
2024-11-22 10:58:54 -0500

    feat(PaymentMethod): transform complete.

--- WebApplication3/user-payment-methods/Interfaces/REST/Transform/CreatePaymentMethodFromResourceAssembler.cs
+++ WebApplication3/user-payment-methods/Interfaces/REST/Transform/CreatePaymentMethodFromResourceAssembler.cs
@@ -1,6 +1,20 @@
+using WebApplication3.user_payment_methods.Domain.Model.Commands;
+using WebApplication3.user_payment_methods.Interfaces.REST.Resources;
+
 namespace WebApplication3.user_payment_methods.Interfaces.REST.Transform;
 
-public class CreatePaymentMethodFromResourceAssembler
+public static class CreatePaymentMethodFromResourceAssembler
 {
-    
+    public static CreateUserPaymentCommand ToCommandFromResource(
+        CreatePaymentMethodResource resource)
+    {
+        return new CreateUserPaymentCommand(
+            resource.CardNumber,
+            resource.ExpirationDate,
+            resource.Cvv,
+            resource.CardHolder,
+            resource.CaregiverId,
+            resource.TutorId
+        );
+    }
 }
--- WebApplication3/user-payment-methods/Interfaces/REST/Transform/PaymentMethodResourceFromEntityAssembler.cs
+++ WebApplication3/user-payment-methods/Interfaces/REST/Transform/PaymentMethodResourceFromEntityAssembler.cs
@@ -1,6 +1,20 @@
+using WebApplication3.user_payment_methods.Domain.Model.Aggregates;
+using WebApplication3.user_payment_methods.Interfaces.REST.Resources;
+
 namespace WebApplication3.user_payment_methods.Interfaces.REST.Transform;
 
-public class PaymentMethodResourceFromEntityAssembler
+public static class PaymentMethodResourceFromEntityAssembler
 {
-    
+    public static PaymentMethodResource ToResourceFromEntity(UserPaymentMethod entity)
+    {
+        return new PaymentMethodResource(
+            entity.Id,
+            entity.CardNumber,
+            entity.ExpirationDate,
+            entity.Cvv,
+            entity.CardHolder,
+            entity.CaregiverId,
+            entity.TutorId
+        );
+    } 
 }
