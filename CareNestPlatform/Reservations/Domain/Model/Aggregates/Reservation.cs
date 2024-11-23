c525b638d4d9265919c352783799944c17936a95
Gabo <u20201e889@upc.edu.pe>
2024-11-23 05:24:52 -0500

    feat: Reservation.cs

---
+++ CareNestPlatform/Reservations/Domain/Model/Aggregates/Reservation.cs
@@ -0,0 +1,24 @@
+﻿namespace CarNest.Reservations.Domain.Model.Aggregates{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int CaregiverId { get; set; }
+        public int TutorId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+        public decimal TotalFare { get; set; }
+
+        private Reservation() { }
+
+        public Reservation(int caregiverId, int tutorId, DateTime startDate, DateTime endDate, string status, decimal totalFare)
+        {
+            CaregiverId = caregiverId;
+            TutorId = tutorId;
+            StartDate = startDate;
+            EndDate = endDate;
+            Status = status;
+            TotalFare = totalFare;
+        }
+    }
+}
