402a8ae49858542307ad6e6aa27b00baba7e86e5
Gabo <u20201e889@upc.edu.pe>
2024-11-23 05:24:22 -0500

    feat: IReservationRepository.cs.

---
+++ CareNestPlatform/Reservations/Domain/Repositories/IReservationRepository.cs
@@ -0,0 +1,10 @@
+﻿using CarNest.Reservations.Domain.Model.Aggregates;
+using WebApplication3.Shared.Domain.Repositories;
+
+namespace CarNest.Reservations.Domain.Repositories
+{
+    public interface IReservationRepository : IBaseRepository<Reservation>
+    {
+        Task<Reservation?> FindReservationByIdAsync(int id);
+    }
+}
