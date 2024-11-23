51a346ad4d26b9faed25b7536ad110bcbc6e6057
Gabo <u20201e889@upc.edu.pe>
2024-11-23 05:22:26 -0500

    feat: CreateReservationCommand.cs

---
+++ CareNestPlatform/Reservations/Domain/Model/Commands/CreateReservationCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace CarNest.Reservations.Domain.Model.Commands
+{
+    public record CreateReservationCommand(
+        int CaregiverId,
+        int TutorId,
+        DateTime StartDate,
+        DateTime EndDate,
+        string Status,
+        decimal TotalFare
+    );
+}
