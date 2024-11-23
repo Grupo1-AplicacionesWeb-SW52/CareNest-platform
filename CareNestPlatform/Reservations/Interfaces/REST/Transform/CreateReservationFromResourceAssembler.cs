f61b88d21111cd4b32f955864eb8f914c2830940
Gabo <u20201e889@upc.edu.pe>
2024-11-23 05:22:44 -0500

    feat: CreateReservationFromResourceAssembler.cs

---
+++ CareNestPlatform/Reservations/Interfaces/REST/Transform/CreateReservationFromResourceAssembler.cs
@@ -0,0 +1,20 @@
+﻿using CarNest.Reservations.Domain.Model.Commands;
+using CarNest.Reservations.Interfaces.REST.Resources;
+
+namespace CarNest.Reservations.Interfaces.REST.Transform
+{
+    public static class CreateReservationFromResourceAssembler
+    {
+        public static CreateReservationCommand ToCommandFromResource(CreateReservationResource resource)
+        {
+            return new CreateReservationCommand(
+                resource.CaregiverId,
+                resource.TutorId,
+                resource.StartDate,
+                resource.EndDate,
+                resource.Status,
+                resource.TotalFare
+            );
+        }
+    }
+}
