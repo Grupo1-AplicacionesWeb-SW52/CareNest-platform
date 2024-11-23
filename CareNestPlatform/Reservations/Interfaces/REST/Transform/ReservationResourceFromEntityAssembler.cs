cff0620441529472e474542198ef3a4b9de88967
Gabo <u20201e889@upc.edu.pe>
2024-11-23 05:26:09 -0500

    feat: ReservationResourceFromEntityAssembler.cs

---
+++ CareNestPlatform/Reservations/Interfaces/REST/Transform/ReservationResourceFromEntityAssembler.cs
@@ -0,0 +1,20 @@
+﻿using CarNest.Reservations.Domain.Model.Aggregates;
+using CarNest.Reservations.Interfaces.REST.Resources;
+
+namespace CarNest.Reservations.Interfaces.REST.Transform{
+    public static class ReservationResourceFromEntityAssembler
+    {
+        public static ReservationResource ToResourceFromEntity(Reservation entity)
+        {
+            return new ReservationResource(
+                entity.Id,
+                entity.CaregiverId,
+                entity.TutorId,
+                entity.StartDate,
+                entity.EndDate,
+                entity.Status,
+                entity.TotalFare
+            );
+        }
+    }
+}
